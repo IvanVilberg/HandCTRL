51e73170454df48dcbadbb05a18f158722e3f5bc
Ivan Vilberg <ivanvilberg@gmail.com>
2025-04-04 22:42:36 +0700

    add config file and refactoring

--- HandMouse.py
+++ HandMouse.py
@@ -1,7 +1,8 @@
 import cv2
-import numpy as np
 import HandTrakingModule as htm
-import time
+import numpy as np
+import json
+import os
 from pynput.mouse import Button, Controller
 from Xlib import display
 # For Windows
@@ -9,10 +10,9 @@ from Xlib import display
 
 
 class HandControl:
-    def __init__(self, wCam=640, hCam=360, smoothening=7 ):
+    def __init__(self, w_cam=640, h_cam=360, config_file="config.json"):
         # Set cam resolution
-        self.wCam, self.hCam = wCam, hCam
-        self.smoothening = smoothening
+        self.wCam, self.hCam = w_cam, h_cam
 
         # For Windows
         # self.user32 = ctypes.windll.user32
@@ -21,8 +21,14 @@ class HandControl:
         screen = display.Display().screen()
         self.wScreen, self.hScreen = screen.width_in_pixels, screen.height_in_pixels
 
-        self.frameReduction = int(self.wCam * 0.1)
-        self.adapterForCam = 50
+
+        # Settings from config file
+        self.config = self._load_config(config_file)
+
+        self.smoothening = self.config["settings"]["smoothening"]
+        self.frameReduction = int(self.wCam * self.config["settings"]["frame_reduction"])
+        self.adapterForCam = self.config["settings"]["adapter_for_cam"]
+        self.delayButtonMax = self.config["settings"]["click_delay"]
 
         self.mouse = Controller()
 
@@ -35,33 +41,47 @@ class HandControl:
 
         self.handDetector = htm.HandDetector(maxHands=1)
 
-        self.delayButton = 0;
-
-        self.centerPoint = hCam // 2
+        self.delayButton = 0
+        self.centerPoint = h_cam // 2
         self.scroll_direction = 0
+
+
+    # Load config file in the program
+    def _load_config(self, config_file):
+        if not os.path.exists(config_file):
+            raise FileNotFoundError(f"Config file {config_file} not found")
+
+        with open(config_file, 'r') as f:
+            config = json.load(f)
+        return config
+
+    # Function for check gestures with config
+    def _check_gesture(self, fingers, gesture_name):
+        gesture = self.config["gestures"][gesture_name]
+
+        if "fingers_up" in gesture:
+            if fingers == gesture["fingers_up"]:
+                return True
+
+        if "min_fingers_up" in gesture and "max_finges_up" in gesture:
+            fingers_up_count = sum(fingers)
+            if gesture["min_fingers_up"] <= fingers_up_count <= gesture["max_fingers_up"]:
+                return True
+
+        return False
+
 
     def process_frame(self, img):
         img = self.handDetector.findHand(img)
-        landmarksList = self.handDetector.findPosition(img, draw=False)
+        landmarks_list = self.handDetector.findPosition(img, draw=False)
 
         if not self.delayButton == 30 and not (self.RightButtonPressed or self.LeftButtonPressed):
             self.delayButton += 1
-        elif self.delayButton == 30:
+        elif self.delayButton == self.delayButtonMax:
             self.RightButtonPressed, self.LeftButtonPressed = False, False
 
 
-        if len(landmarksList) != 0:
-            # Set coordinates for:
-            # Thumb
-            # Index finger
-            # Middle finger
-            x0, y0 = landmarksList[4][1], landmarksList[4][2]
-            x1, y1 = landmarksList[8][1], landmarksList[8][2]
-            x2, y2 = landmarksList[12][1], landmarksList[12][2]
-
-            holdAMoveX1, holdAMoveY1 = landmarksList[9][1], landmarksList[9][2]
-            holdAMoveX2, holdAMoveY2 = landmarksList[0][1], landmarksList[0][2]
-
+        if len(landmarks_list) != 0:
             fingers = self.handDetector.fingersUp()
 
             # Draw rectangle for mouse control
@@ -70,30 +90,62 @@ class HandControl:
                           (255, 0, 0), 2)
 
             # Move mouse if only index finger up
-            if fingers[1] == 1 and fingers[2] == 1 and fingers.count(1) == 2:
+            if self._check_gesture(fingers, "move_mouse"):
+                x1, y1 = landmarks_list[8][1], landmarks_list[8][2]
+                x2, y2 = landmarks_list[12][1], landmarks_list[12][2]
                 cx, cy = (x1 + x2) // 2, (y1 + y2) // 2
                 cv2.circle(img, (cx, cy), 15, (255, 0, 255), cv2.FILLED)
                 self._move_mouse(cx, cy)
 
 
-            # Clicking mode if index and middle fingers are up
-            if fingers[0] == 1 and fingers[4] == 0:
-                self._handle_clicking(img, fingers)
+            # Left click
+            if self._check_gesture(fingers, "left_click"):
+                gesture = self.config["gestures"]["left_click"]
+                length, img, _ = self.handDetector.findDistance(
+                    gesture["landmarks"][0], gesture["landmarks"][1], img)
+
+                if not self.LeftButtonPressed and length < gesture["distance_threshold"]:
+                    self.mouse.click(Button.left, 1)
+                    self.RightButtonPressed, self.LeftButtonPressed = False, True
+                self.clickSmother = 0
+
+            # Right click
+            if self._check_gesture(fingers, "right_click"):
+                gesture = self.config["gestures"]["right_click"]
+                length, img, _ = self.handDetector.findDistance(
+                    gesture["landmarks"][0], gesture["landmarks"][1], img)
+
+                if not self.RightButtonPressed and length < gesture ["distance_threshold"]:
+                    self.mouse.click(Button.right, 1)
+                    self.RightButtonPressed, self.LeftButtonPressed = True, False
+                self.clickSmother = 0
 
             # Hold mode if all fingers down
-            if fingers.count(0) == 5:
+            if self._check_gesture(fingers, "hold_and_move"):
+                x1, y1 = landmarks_list[8][1], landmarks_list[8][2]
+                x2, y2 = landmarks_list[12][1], landmarks_list[12][2]
                 cx, cy = (x1 + x2) // 2, (y1 + y2) // 2
-                self._hold_button_and_move(cx, cy)
 
-            # Stop mode if all fingers up
-            if fingers.count(1) == 5:
+                if not self.LeftButtonPressed:
+                    self.mouse.press(Button.left)
+                    self.LeftButtonPressed = True
+                self._move_mouse(cx, cy)
+
+
+            if self._check_gesture(fingers, "release"):
                 self.mouse.release(Button.left)
 
-            # Scroll mode if only pinky use
-            if fingers[0] == 0 and fingers.count(1) == 4:
-                self._handle_scrolling(landmarksList[20][2])
+            if self._check_gesture(fingers, "scroll"):
+                gesture = self.config["gestures"]["scroll"]
+                y = landmarks_list[gesture["landmark"]][2]
+
+                if y < self.centerPoint - gesture["scroll_threshold"]:
+                    self.mouse.scroll(0, 1)
+                elif y > self.centerPoint + gesture["scroll_threshold"]:
+                    self.mouse.scroll(0, -1)
 
         return img
+
 
     def _move_mouse(self, x1, y1):
         x3 = np.interp(x1, (self.frameReduction, (self.wCam - self.frameReduction)), (0, self.wScreen))
@@ -106,6 +158,7 @@ class HandControl:
 
         self.mouse.position = [(self.wScreen - self.cLocX), self.cLocY]
         self.pLocX, self.pLocY = self.cLocX, self.cLocY
+
 
     def _handle_clicking(self, img, fingers):
         lengthMB, img, lineInfoIM = self.handDetector.findDistance(12, 11, img)
@@ -121,6 +174,7 @@ class HandControl:
             self.RightButtonPressed, self.LeftButtonPressed = True, False
 
         self.clickSmother = 0
+
 
     def _hold_button_and_move(self, cx, cy):
         if not self.LeftButtonPressed:
@@ -145,7 +199,7 @@ def main():
 
     handControl = HandControl(wCam, hCam)
 
-    pTime = 0
+
 
     while True:
         success, img = cap.read()
@@ -153,13 +207,6 @@ def main():
             break
 
         img = handControl.process_frame(img)
-
-        # FPS calculation
-        cTime = time.time()
-        fps = 1 / (cTime - pTime)
-        pTime = cTime
-
-        cv2.putText(img, f"FPS: {int(fps)}", (10, 30), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 255, 0), 2)
 
         # Display
         cv2.imshow("Hand Control", img)
